e75d6ad7ef868eb75a13b3bcafe129153c04be68
Jihyun Yu <yjh0502@gmail.com>
2014-10-06 13:00:08 +0900

    Remove critbit_cow, libjemalloc from run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 
-BINS='art.bin critbit.bin critbit_cow.bin'
+BINS='art.bin critbit.bin'
 make $BINS
 
 echoerr() { echo "$@" 1>&2; }
 
 export TIME="%E %M"
-export LD_PRELOAD=/usr/lib/libjemalloc.so.1
 for bin in $BINS;
 do
     export FILENAME="${bin%.*}"_batch.out
     rm -f $FILENAME
 
     echoerr "$bin"
-    for iter in 100000 333333 1000000;# 3333333 10000000;
+    for iter in 100000 333333 1000000 3333333 10000000 33333333 100000000;
     do
         ITER=$iter time ./$bin 2>&1 | tee -a $FILENAME
     done
